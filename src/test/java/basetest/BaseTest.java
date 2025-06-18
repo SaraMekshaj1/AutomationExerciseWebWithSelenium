da879ca97253beb573b4a793851f5ba690d06e64
Sara <saramekshaj17@gmal.com>
2025-06-18 14:00:13 +0200

    Resources/Sreenshots folder was added , also in BaseTest Class was added takeFailedResultScreenshot after method wich encbles taking screenshots when the test fails

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -2,11 +2,20 @@ package basetest;
 
 import base.BasePage;
 import home.HomePage;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.internal.TestResult;
+
+import java.io.File;
+import java.io.IOException;
 
 import static utilities.Utility.setUtilityDriver;
 
@@ -31,6 +40,25 @@ public class BaseTest {
         setUtilityDriver();
 
     }
+
+    @AfterMethod
+    public void takeFailedResultScreenshot(ITestResult testResult){
+        if (ITestResult.FAILURE== testResult.getStatus()){
+            TakesScreenshot screenshot= (TakesScreenshot)driver ;
+            File source=  screenshot.getScreenshotAs(OutputType.FILE);
+            File destination=new File(System.getProperty("user.dir") +
+                    "/resources/screenshots/("+
+                    java.time.LocalDate.now()+
+                    testResult.getName() +".png");
+            try {
+                FileHandler.copy(source,destination);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Screenshot Located at "+destination);
+        }
+    }
+
 
     @AfterClass
     public void tearDown(){
